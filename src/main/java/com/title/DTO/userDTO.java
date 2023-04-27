ffec678a9fc722df8eb577e9cfee85fced62ddf7
박준영 <wnsdud6969@naver.com>
2023-04-27 21:59:37 +0900

    feat: userDTO 추가

---
+++ src/main/java/com/title/DTO/userDTO.java
@@ -0,0 +1,51 @@
+package com.title.DTO;
+
+public class userDTO {
+
+  int userId;
+  int age;
+  String gender;
+  String occupation;
+  String ZIPCODE;
+
+  public int getUserId() {
+    return userId;
+  }
+
+  public void setUserId(int userId) {
+    this.userId = userId;
+  }
+
+  public int getAge() {
+    return age;
+  }
+
+  public void setAge(int age) {
+    this.age = age;
+  }
+
+  public String getGender() {
+    return gender;
+  }
+
+  public void setGender(String gender) {
+    this.gender = gender;
+  }
+
+  public String getOccupation() {
+    return occupation;
+  }
+
+  public void setOccupation(String occupation) {
+    this.occupation = occupation;
+  }
+
+  public String getZIPCODE() {
+    return ZIPCODE;
+  }
+
+  public void setZIPCODE(String ZIPCODE) {
+    this.ZIPCODE = ZIPCODE;
+  }
+
+}
